c905d2a27afb2d2ea0619ed853ee53bf1b142a1d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-05 10:25:56 +0300

    refactor: separating variants

---
+++ src/components/elements/UI/Button/Button.tsx
@@ -0,0 +1,20 @@
+import { forwardRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { type VariantProps } from 'class-variance-authority';
+import { cn } from '@/libs/cn';
+import { buttonVariants } from '@/elements/UI/Button/variants';
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  };
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
