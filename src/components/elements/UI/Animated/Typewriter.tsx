286e98af8dad2044951da9253b1ccebeca784626
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-05 10:26:06 +0300

    feat: adding custom typewriter effect with framer

---
+++ src/components/elements/UI/Animated/Typewriter.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, ReactElement, ReactNode } from 'react';
+import { motion, MotionProps } from 'framer-motion';
+
+type TypewriterProps = MotionProps & {
+  text: string;
+  as?: React.ElementType;
+  children?: ReactNode;
+};
+
+const Typewriter: React.FC<TypewriterProps> = ({ text, children, as: Component = 'div', ...motionProps }) => {
+  const [typewriterText, setTypewriterText] = useState('');
+  const [typingCompleted, setTypingCompleted] = useState(false);
+
+  useEffect(() => {
+    let index = 0;
+
+    const typeInterval = setInterval(() => {
+      if (index < text.length) {
+        setTypewriterText(text.substring(0, index + 1));
+        index += 1;
+      } else {
+        setTypingCompleted(true);
+        clearInterval(typeInterval);
+      }
+    }, 100);
+
+    return () => clearInterval(typeInterval);
+  }, [text]);
+
+  return (
+    <motion.div {...motionProps}>
+      <Component>
+        {typewriterText}
+        {!typingCompleted && <span className="text-opacity-50 animate-blink">_</span>}
+        {children}
+      </Component>
+    </motion.div>
+  );
+};
+
+export default Typewriter;
