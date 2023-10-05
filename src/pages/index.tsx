6133033cb2dab3db2611c3dd2094384bf06da3cf
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-05 10:30:49 +0300

    chore: adding link for easy level

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Waves from '@/assets/layered-waves.svg';
 import StaggeredFadeIn from '@/elements/UI/Animated/StaggeredFadeIn';
-import { Button } from '@/elements/UI/Button';
+import { Button } from '@/components/elements/UI/Button/Button';
 
 import Landing from '@/components/modules/Home/Landing';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
       <section className="mb-60">
         <Landing />
         <StaggeredFadeIn delay={3.5} className="flex flex-col items-center mt-12 gap-4">
-          <Button variant="outline" size="lg" className="w-fit">
-            New Game
+          <Button asChild variant="default" size="lg" className="w-fit">
+            <Link href="/easyLevel?level=1">Easy Level</Link>
           </Button>
           <Button variant="default" size="lg" className="w-fit">
             New Game
