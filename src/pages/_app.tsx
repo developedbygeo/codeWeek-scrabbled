db60ea11dd83ca8c3da03619d0816efb7aeb86fb
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-06 19:04:51 +0300

    chore: conditional rendering for LevelLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-import Layout from '@/components/elements/UI/Layout';
+import Layout from '@/elements/UI/Layout';
+import LevelLayout from '@/elements/UI/LevelLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
+  if ('isLevel' in Component && Component.isLevel) {
+    return (
+      <LevelLayout>
+        <Component {...pageProps} />
+      </LevelLayout>
+    );
+  }
   return (
     <Layout>
       <Component {...pageProps} />
