a8d0ff5766634b2fff0235e01c75c2099019dd25
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-06 19:04:22 +0300

    style: button scale update

--- src/components/elements/UI/Button/variants.ts
+++ src/components/elements/UI/Button/variants.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center active:scale-95 justify-center transition-[transform_colors] duration-200 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -15,7 +15,7 @@ export const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
+        lg: 'h-12 rounded-md py-3 px-8',
         icon: 'h-9 w-9',
       },
     },
