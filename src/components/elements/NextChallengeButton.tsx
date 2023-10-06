877fb1497acca2cfb2b30ca36e3f5f99e3dff1f6
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-06 19:03:35 +0300

    feat: client-side nav

---
+++ src/components/elements/NextChallengeButton.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { CommonProps } from '@/types/general';
+
+import { Button } from '@/elements/UI/Button/Button';
+
+type NextChallengeButtonProps = CommonProps & {
+  level: number;
+  maxLevel: number;
+};
+
+const NextChallengeButton = ({ className, level, maxLevel }: NextChallengeButtonProps) => {
+  const { pathname, asPath, query } = useRouter();
+  const shouldRenderNextLevel = level < maxLevel && query.wordFound === 'true';
+  const currentLevel = query.level;
+  const isCurrentLevelValid = !isNaN(Number(currentLevel)) && Number(currentLevel) > 0;
+  const nextLevel = Number(currentLevel) + 1;
+
+  console.log(query.wordFound);
+
+  return shouldRenderNextLevel && isCurrentLevelValid ? (
+    <Button className={className} size="lg" asChild>
+      <Link href={`${pathname}?level=${nextLevel}`}>Επόμενο επίπεδο</Link>
+    </Button>
+  ) : null;
+};
+
+export default NextChallengeButton;
