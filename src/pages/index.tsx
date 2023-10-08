b0c03297f758441baaa21bc9b2616c0ce45c0c83
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-08 17:49:36 +0200

    chore: adding todos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,25 @@ export default function Home() {
     <div className="relative h-screen w-screen flex flex-col items-center justify-center">
       <section className="mb-60">
         <Landing />
-        <StaggeredFadeIn delay={3.5} className="flex flex-col items-center mt-12 gap-4">
-          <Button asChild variant="default" size="lg" className="w-fit">
-            <Link href="/easyLevel?level=1">Easy Level</Link>
+        <StaggeredFadeIn
+          delay={3.5}
+          className="flex flex-col items-center mt-12 gap-4"
+          wrapperClassName="w-1/3"
+        >
+          <Button asChild variant="default" size="lg" className="w-full bg-[#fa7268]">
+            <Link href="/easyLevel?level=1">Easy</Link>
           </Button>
-          <Button variant="default" size="lg" className="w-fit">
-            New Game
+          {/* TODO need to fix hover color */}
+          <Button asChild variant="default" size="lg" className="w-full bg-[#e34c67]">
+            <Link href="/mediumLevel?level=1">Medium</Link>
           </Button>
-          <Button variant="outline" size="lg" className="w-fit">
-            New Game
+          {/* TODO need to fix hover color */}
+          <Button asChild variant="default" size="lg" className="w-full bg-[#c62368]">
+            <Link href="/hardLevel?level=1">Hard</Link>
           </Button>
         </StaggeredFadeIn>
       </section>
-      <Waves className="absolute bottom-0 w-screen pointer-events-none" />
+      <Waves className="absolute bottom-0 w-screen left-0 pointer-events-none" />
     </div>
   );
 }
