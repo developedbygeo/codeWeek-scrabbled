d0ede82862262cce3cc9b635f612bf97e0c43c1f
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-08 22:09:22 +0200

    chore: using leafAnimation hook

--- src/components/elements/UI/Animated/AnimatedMinions.tsx
+++ src/components/elements/UI/Animated/AnimatedMinions.tsx
@@ -4,11 +4,13 @@ import DonutLove from '@/assets/donut_love.svg';
 
 import useFollowMouse from '@/hooks/useFollowMouse';
 import { CommonProps } from '@/types/general';
+import useLeafAnimation from '@/hooks/useLeafAnimation';
 
 const AnimatedMinions = ({ className }: CommonProps) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useFollowMouse({ containerRef, eyeRadius: 5 });
+  useLeafAnimation({ containerRef });
 
   return (
     <div className={className} ref={containerRef}>
