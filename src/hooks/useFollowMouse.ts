e2c8880608ecae4068bca69ab224b929f3a1b2a5
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-09 01:05:26 +0200

    perf: debouncing and cleanup

--- src/hooks/useFollowMouse.ts
+++ src/hooks/useFollowMouse.ts
@@ -1,4 +1,5 @@
-import { RefObject, useEffect } from 'react';
+import { RefObject, useCallback, useEffect } from 'react';
+import _ from 'lodash';
 
 type FollowMouseOptions = {
   containerRef: RefObject<HTMLElement | null>;
@@ -6,44 +7,43 @@ type FollowMouseOptions = {
 };
 
 const useFollowMouse = ({ containerRef, eyeRadius = 5 }: FollowMouseOptions) => {
-  const handleMouseMove = (e: MouseEvent) => {
-    const svgContainer = containerRef.current;
-    if (!svgContainer) return;
+  const handleMouseMove = useCallback(
+    _.debounce((e: MouseEvent) => {
+      const svgContainer = containerRef.current;
+      if (!svgContainer) return;
 
-    // Find the center of the container
-    const rect = svgContainer.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
+      // Find the center of the container
+      const rect = svgContainer.getBoundingClientRect();
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
 
-    // Determine the angle from the center of the container to the mouse
-    const deltaX = e.clientX - centerX;
-    const deltaY = e.clientY - centerY;
-    const angle = Math.atan2(deltaY, deltaX);
-
-    // Convert the angle to an eye offset
-    const offsetX = Math.cos(angle) * eyeRadius;
-    const offsetY = Math.sin(angle) * eyeRadius;
-
-    const eyeGroup = svgContainer.querySelector('#eyes');
-    const eyeElements = eyeGroup?.querySelectorAll('path') || [];
-
-    eyeElements.forEach((eye, index) => {
-      const eyeBox = eye.getBBox();
-      const eyeCenterX = eyeBox.x + eyeBox.width / 2;
-      const eyeCenterY = eyeBox.y + eyeBox.height / 2;
-
-      // Check angle from the center of the eye to the mouse
-      const deltaX = e.clientX - (svgContainer.getBoundingClientRect().left + eyeCenterX);
-      const deltaY = e.clientY - (svgContainer.getBoundingClientRect().top + eyeCenterY);
+      // Determine the angle from the center of the container to the mouse
+      const deltaX = e.clientX - centerX;
+      const deltaY = e.clientY - centerY;
       const angle = Math.atan2(deltaY, deltaX);
 
-      // Converting angle to offset in relation to the eye
-      const offsetX = Math.cos(angle) * eyeRadius;
-      const offsetY = Math.sin(angle) * eyeRadius;
+      const eyeGroup = svgContainer.querySelector('#eyes');
+      const eyeElements = eyeGroup?.querySelectorAll('path') || [];
 
-      eye.setAttribute('transform', `translate(${offsetX}, ${offsetY})`);
-    });
-  };
+      eyeElements.forEach((eye, index) => {
+        const eyeBox = eye.getBBox();
+        const eyeCenterX = eyeBox.x + eyeBox.width / 2;
+        const eyeCenterY = eyeBox.y + eyeBox.height / 2;
+
+        // Check angle from the center of the eye to the mouse
+        const deltaX = e.clientX - (svgContainer.getBoundingClientRect().left + eyeCenterX);
+        const deltaY = e.clientY - (svgContainer.getBoundingClientRect().top + eyeCenterY);
+        const angle = Math.atan2(deltaY, deltaX);
+
+        // Converting angle to offset in relation to the eye
+        const offsetX = Math.cos(angle) * eyeRadius;
+        const offsetY = Math.sin(angle) * eyeRadius;
+
+        eye.setAttribute('transform', `translate(${offsetX}, ${offsetY})`);
+      });
+    }, 2),
+    [containerRef.current, eyeRadius]
+  );
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove);
