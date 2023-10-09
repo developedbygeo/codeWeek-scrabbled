94539bf22106cf488415af7032863a8155b988f7
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-09 21:04:20 +0200

    chore: adding head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 import Layout from '@/elements/UI/Layout';
@@ -7,14 +8,24 @@ import LevelLayout from '@/elements/UI/LevelLayout';
 export default function App({ Component, pageProps }: AppProps) {
   if ('isLevel' in Component && Component.isLevel) {
     return (
-      <LevelLayout>
-        <Component {...pageProps} />
-      </LevelLayout>
+      <>
+        <Head>
+          <title>ScrabbleEd</title>
+        </Head>
+        <LevelLayout>
+          <Component {...pageProps} />
+        </LevelLayout>
+      </>
     );
   }
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>ScrabbleEd</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
