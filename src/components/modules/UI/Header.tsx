0a2f8ffe1af4d18de1cefba7b80380b338434423
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-09 21:54:00 +0200

    chore: adding createdBy

--- src/components/modules/UI/Header.tsx
+++ src/components/modules/UI/Header.tsx
@@ -11,13 +11,16 @@ type HeaderProps = CommonProps & {
 const Header = ({ className, shouldRenderHomeLink }: HeaderProps) => {
   return (
     <header className={`${className} w-screen flex justify-between items-center h-[8vh] px-10 py-5`}>
-      <div className="relative h-[5vh] aspect-square w-60">
-        <Image
-          src="/codeweek-logo.png"
-          alt="Codeweek logo"
-          style={{ objectFit: 'contain', backgroundRepeat: 'no-repeat' }}
-          fill
-        />
+      <div className="flex gap-4 items-center">
+        <div className="relative h-[5vh] aspect-square w-60">
+          <Image
+            src="/codeweek-logo.png"
+            alt="Codeweek logo"
+            style={{ objectFit: 'contain', backgroundRepeat: 'no-repeat' }}
+            fill
+          />
+        </div>
+        <p className="font-light  text-gray-500 font-sans text-xs">Created by: K. Vgena & G. Anagnostou</p>
       </div>
       {shouldRenderHomeLink && (
         <Button asChild>
